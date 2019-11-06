8650713efb3652dc5e37a47f4e230f99a7522970
Peter Kim <pkim017@gmail.com>
2019-11-06 11:26:59 -0500

    Initial Commit

---
+++ Peter's Contact List/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Peter's Contact List
+//
+//  Created by Peter Kim on 11/6/19.
+//  Copyright © 2019 Peter Kim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Peter's Contact List/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Peter's Contact List
+//
+//  Created by Peter Kim on 11/6/19.
+//  Copyright © 2019 Peter Kim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
